f02c496e5fac875e550c40cfc98a40fc1dfb05c0
Ned Rockson <nedrocks@gmail.com>
2021-08-03 19:22:13 -0700

    Actually correct syntax

--- main.tf
+++ main.tf
@@ -269,12 +269,12 @@ resource "aws_launch_template" "bastion_launch_template" {
 
   tag_specifications {
     resource_type = "instance"
-    tags          = merge(tomap({"Name", var.bastion_launch_template_name}), merge(var.tags))
+    tags          = merge(tomap({"Name" = var.bastion_launch_template_name}), merge(var.tags))
   }
 
   tag_specifications {
     resource_type = "volume"
-    tags          = merge(tomap({"Name", var.bastion_launch_template_name}), merge(var.tags))
+    tags          = merge(tomap({"Name" = var.bastion_launch_template_name}), merge(var.tags))
   }
 
   lifecycle {
